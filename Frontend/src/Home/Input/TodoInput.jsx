fd36ae0d90ffee7464ea7d7703f507c26cb130d6
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-11-04 16:44:30 +0530

    To-Do input now fully complete

--- Frontend/src/Home/Input/TodoInput.jsx
+++ Frontend/src/Home/Input/TodoInput.jsx
@@ -1,15 +1,60 @@
+import axios from "axios";
+import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 function TodoInput() {
+  const [inputData, setInputData] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const createTodo = async (e) => {
+    e.preventDefault();
+
+    if (!inputData.trim()) return toast.error("Please enter a task!");
+
+    // create a loading toast
+    const toastId = toast.loading("Creating To-Do...");
+
+    try {
+      const res = await axios.post("http://localhost:4000/api/v1/users/to-do", {
+        inputData,
+      });
+      console.log("To-Do Created successfully!", res.data);
+
+      toast.update(toastId, {
+        render: "To-Do Created successfully!",
+        type: "success",
+        isLoading: false,
+        autoClose: 2000,
+      });
+
+      setInputData("");
+    } catch (error) {
+      console.log(error);
+
+      toast.update(toastId, {
+        render: "Failed to create To-Do",
+        type: "error",
+        isLoading: false,
+        autoClose: 2000,
+      });
+    }
+  };
+
   return (
     <div className="flex gap-3 items-center">
       <input
         type="text"
+        value={inputData}
+        onChange={(e) => setInputData(e.target.value)}
         placeholder="Add a new task..."
-        className="border pl-2 pr-20 py-1 text-lg rounded"
+        className="w-[320px] border border-green-700 px-2 py-1 text-lg rounded outline-none tracking-wider font-serif"
       />
-      <button className="w-11 h-8 bg-green-500 hover:bg-green-600 text-white flex justify-center items-center rounded cursor-pointer">
-        <FaPlus size={18} />
+      <button
+        onClick={createTodo}
+        className="w-14 h-9 bg-green-600/80 hover:bg-green-600 text-white flex justify-center items-center rounded cursor-pointer transition duration-200 ease-in-out"
+      >
+        <FaPlus size={17} />
       </button>
     </div>
   );
