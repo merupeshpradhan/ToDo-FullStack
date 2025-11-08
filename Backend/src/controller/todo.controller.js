146f2e2f0710dc940d5e4d5f9e1a38f9eb232685
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-11-08 10:40:38 +0530

    Now create delete function fo To-Do

--- Backend/src/controller/todo.controller.js
+++ Backend/src/controller/todo.controller.js
@@ -7,14 +7,30 @@ const createTodo = asyncHandler(async (req, res) => {
   const { title } = req.body;
 
   if (!title || title.trim() === "") {
-    throw new ApiError(401, "Please write something for your To-Do");
+    throw new ApiError(401, "Please write something for your To-Do.");
   }
 
   const todo = await Todo.create({ title, user: req.user._id });
 
   return res
     .status(201)
-    .json(new ApiResponse(201, todo, "Successfully created Todo"));
+    .json(new ApiResponse(201, todo, "Successfully created Todo."));
 });
 
-export { createTodo };
+const deleteTodo = asyncHandler(async (req, res) => {
+  const { todoId } = req.params;
+
+  const todo = await Todo.findById(todoId);
+  if (!todo) {
+    throw new ApiError(401, "To-Do not found.");
+  }
+
+  // Delete To-Do from DB
+  await Todo.findByIdAndDelete(todoId);
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "To-Do has been successfully deleted."));
+});
+
+export { createTodo, deleteTodo };
--- Backend/src/routes/todo.routes.js
+++ Backend/src/routes/todo.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTodo } from "../controller/todo.controller.js";
+import { createTodo, deleteTodo } from "../controller/todo.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/").post(authMiddleware, createTodo);
+router.route("/:todoId").delete(authMiddleware, deleteTodo);
 
 export default router;
